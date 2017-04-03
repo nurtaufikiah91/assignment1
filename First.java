da4b1254251dad395b19bae53eadf5a9fe5e5b5e
nurtaufikiah91 <ntcm_91@yahoo.com>
2017-04-03 17:27:19 +0800

    Update First.java

--- First.java
+++ First.java
@@ -1,17 +1,19 @@
-package org.kodejava.example.io;
+package first;
 
 import java.io.*;
 
-public class First {
+/**
+ *
+ * @author Nurtaufikiah
+ */
+public class firstFirst {
     public static void main(String[] args) {
-        File file = new File("First.txt");
+        File file = new File("first.txt");
 
         try (Writer writer = new BufferedWriter(new FileWriter(file))) {
-            String contents = " A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
-            writer.write(contents);
-            } 
-           catch (IOException e) {
-            e.printStackTrace();
+            String contents = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+             writer.write(contents);
+        } catch (IOException e) {
         }
     }
 }
